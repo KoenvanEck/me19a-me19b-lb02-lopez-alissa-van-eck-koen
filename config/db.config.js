6bb725c0075aabae05c99c811eb7b092bd9f501c
KoenvanEck <62997504+KoenvanEck@users.noreply.github.com>
2022-01-25 19:29:14 +0100

    Delete config directory

--- config/db.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    HOST: "localhost",
-    USER: "1admin",
-    PASSWORD: "admin",
-    DB: "bzzDB"
-}
--- config/httpcodes.config.js
+++
@@ -1,5 +0,0 @@
-exports.SUCCESSFUL_CREATED = 201;
-exports.BAD_REQUEST = 400;
-exports.NOT_FOUND = 404;
-exports.NOT_ACCEPTABLE = 406;
-exports.SERVER_ERROR = 500;
